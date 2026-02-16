a2f532106628aad8e3c065bd4382e112d28db48e
Aparna <aparna21nr@gmail.com>
2026-02-16 21:10:04 +0530

    added appolo mission audio with pause and play to make sound

--- script.js
+++ script.js
@@ -18,11 +18,32 @@ function generateStars(container, count) {
 
   container.style.background = stars.join(",");
 }
-
 document.addEventListener("DOMContentLoaded", function() {
   const layer1 = document.querySelector(".layer1");
   const layer2 = document.querySelector(".layer2");
+  generateStars(layer1, 150);  
+  generateStars(layer2, 60);  
 
-  generateStars(layer1, 150);  // distant stars
-  generateStars(layer2, 60);  // closer stars
+  const audio = document.getElementById("myAudio");
+  const button = document.querySelector(".soundstatus button");
+
+  if (button) { // check exists
+    const span = button.querySelector("span");
+   
+
+    button.addEventListener("click", function() {
+      if (span.textContent === "❚❚") {
+        audio.pause();
+        span.textContent = "▶";
+        button.nextSibling.textContent = " SOUND OFF";
+       
+      } else {
+        audio.play().catch(err => console.log("Autoplay blocked:", err));
+        span.textContent = "❚❚";
+                button.nextSibling.textContent = " SOUND ON";
+
+      
+      }
+    });
+  }
 });
