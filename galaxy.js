d6c683e96aea344936201a19187d803868f84cf0
Aparna <aparna21nr@gmail.com>
2026-02-22 18:53:09 +0530

    made a mistake before, corrected

--- galaxy.js
+++ galaxy.js
@@ -28,3 +28,25 @@ document.addEventListener("DOMContentLoaded", function() {
     celesta_galaxy(layer3, 90,1);
 
 });
+
+
+//now meteorshower
+const sky = document.querySelector(".nightsky");
+
+function createMeteor() {
+for (let i = 0; i < 3; i++) {
+  const meteor = document.createElement("div");
+  meteor.classList.add("meteorshower");
+
+   meteor.style.left = Math.random() * window.innerWidth + "px";//important to use window.innerWidth to get the actual width of the viewport, i didnt know it
+    meteor.style.top = "-50px"
+  meteor.style.animationDelay = Math.random() * 5 + "s";
+
+  sky.appendChild(meteor);
+    setTimeout(() => {
+    meteor.remove();
+  }, 4000);
+}
+
+}
+setInterval(createMeteor, 5000);
