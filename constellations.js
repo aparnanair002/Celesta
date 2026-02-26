88dfb1420a7fcfc7b098bd82ac9fa218c2ebb0d8
Aparna <aparna21nr@gmail.com>
2026-02-26 21:21:31 +0530

    added all the constellation names

---
+++ constellations.js
@@ -0,0 +1,33 @@
+const constels= document.querySelector('.constels');
+
+document.addEventListener('DOMContentLoaded', () => {
+    constellations=['Andromeda', 'Antlia', 'Apus', 'Aquarius', 'Aquila', 'Ara', 'Aries', 'Auriga', 'Boötes', 'Caelum', 
+        'Camelopardalis', 'Cancer', 'Canes Venatici', 'Canis Major', 'Canis Minor', 
+        'Capricornus', 'Carina', 'Cassiopeia', 'Centaurus', 'Cepheus', 'Cetus',
+         'Chamaeleon', 'Circinus', 'Columba', 'Coma Berenices', 'Corona Australis', 
+         'Corona Borealis', 'Corvus', 'Crater', 'Crux', 'Cygnus', 'Delphinus', 
+         'Dorado', 'Draco', 'Equuleus', 'Eridanus', 'Fornax', 'Gemini', 'Grus', 
+         'Hercules', 'Horologium', 'Hydra', 'Hydrus', 'Indus', 'Lacerta', 'Leo', 
+         'Leo Minor', 'Lepus', 'Libra', 'Lupus', 'Lynx', 'Lyra', 'Mensa', 'Microscopium', 
+         'Monoceros', 'Musca', 'Norma', 'Octans', 'Ophiuchus', 'Orion', 'Pavo', 'Pegasus', 
+         'Perseus', 'Phoenix', 'Pictor', 'Pisces', 'Piscis Austrinus', 'Puppis', 'Pyxis', 
+         'Reticulum', 'Sagitta', 'Sagittarius', 'Scorpius', 'Sculptor', 'Scutum', 'Serpens', 
+         'Sextans', 'Taurus', 'Telescopium', 'Triangulum', 'Triangulum Australe', 'Tucana', 
+         'Ursa Major', 'Ursa Minor', 'Vela', 'Virgo', 'Volans', 'Vulpecula'];
+    represesntations=['👸', '🛠️', '🐦', '🏺', '🦅', '🔥', '🐏', '🚗', '🧑‍🌾', '🔪', '🦒', '🦀', 
+        '🐕🐕', '🐕', '🐕', '🐐', '🚢', '👑', '🐎', '👑', '🐉', '🦎', '📐', '🕊️', '💇‍♀️', 
+        '👑', '👑', '🐦‍⬛', '🏆', '✝️', '🦢', '🐬', '🐟', '🐉', '🐎', '🌊', '🔥', '👯', 
+        '🐦', '🦸', '🕰️', '🐍', '🐍', '🧑', '🦎', '🦁', '🦁', '🐇', '⚖️', '🐺', '🐆', 
+        '🎵', '🏔️', '🔬', '🦄', '🪰', '📐', '🧭', '🐍', '🏹', '🦚', '🐎', '🗡️', '🔥', 
+        '🎨', '🐟🐟', '🐟', '🚢', '🧭', '🕸️', '🏹', '🏹', '🦂', 
+        '🗿', '🛡️', '🐍', '📐', '🐂', '🔭', '🔺', '🔺', '🐦', 
+        '🐻', '🐻', '⛵', '👩', '🐟', '🦊']
+    for(let i=0; i<88; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        
+        row.innerHTML = '<b>'+(i+1)+')    '+represesntations[i]+'  '+constellations[i] +' </b>';
+
+        constels.appendChild(row);
+    }
+    });
