dfe6d142745dea7adbcf49c0e49754ec2c842e87
Aparna <aparna21nr@gmail.com>
2026-02-27 22:45:11 +0530

    added all the constellation names

--- constellations.js
+++ constellations.js
@@ -22,12 +22,11 @@ document.addEventListener('DOMContentLoaded', () => {
         '🎨', '🐟🐟', '🐟', '🚢', '🧭', '🕸️', '🏹', '🏹', '🦂', 
         '🗿', '🛡️', '🐍', '📐', '🐂', '🔭', '🔺', '🔺', '🐦', 
         '🐻', '🐻', '⛵', '👩', '🐟', '🦊']
-    for(let i=0; i<88; i++){
-        const row = document.createElement('div');
-        row.classList.add('row');
-        
-        row.innerHTML = '<b>'+(i+1)+')    '+represesntations[i]+'  '+constellations[i] +' </b>';
-
-        constels.appendChild(row);
-    }
-    });
+                        for(let i=0; i<88; i++){
+                            const row = document.createElement('div');
+                            row.classList.add('row');
+                            
+                            row.innerHTML = '<b>'+(i+1)+')  '+represesntations[i]+'  '+constellations[i] +' </b>';
+                            constels.appendChild(row);
+                        }
+                        });
