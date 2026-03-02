ce9309c7f0857318221df6149ade50d4eb86e832
Aparna <aparna21nr@gmail.com>
2026-03-02 22:25:43 +0530

    added details on the pop up, more to be followed

--- constellations.js
+++ constellations.js
@@ -7,32 +7,45 @@ document.addEventListener('DOMContentLoaded', () => {
         '🎨', '🐟🐟', '🐟', '🚢', '🧭', '🕸️', '🏹', '🏹', '🦂', 
         '🗿', '🛡️', '🐍', '📐', '🐂', '🔭', '🔺', '🔺', '🐦', 
         '🐻', '🐻', '⛵', '👩', '🐟', '🦊']
+
+
+  images_hh=['andromeda.png', 'antlia.png', 'aquarius.png', 'aqua.png', 'aquila.png', 'aries.png', 'taurus.png', 'gemini.png', 'cancer.png', 'leo.png', 'virgo.png', 'libra.png', 'scorpio.png', 'sagittarius.png', 'capricornus.png', 'aquarius.png', 'pisces.png', 'pisces.png', 
+  'carina.png', 'cassiopeia.png', 'centaurus.png', 'cepheus.png', 'cygnus.png', 'delphinus.png', 'draco.png', 'equuleus.png',
+   'eridanus.png', 'fornax.png', 'gemini.png', 'grus.png', 'hercules.png', 'horologium.png', 'hydra.png', 'hydrus.png',
+    'indus.png', 'lacerta.png', 'leo minor.png', 'leoninae.png', 'lupus.png', 'lynx.png', 'lyra.png', 
+    'mensae.png','microscopium.png','monoceros.png','musca.png','norma.png','octans.png','ophiuchus.png','orion.png','pavo.png','pegasus.png','perseus.png','phoenix.png','pictor.png', 'pisces.png','piscis austrinus.png','puppis.png','pyxis.png','reticulum.png','sagitta.png','sagittarius.png','scorpius.png','sculptor.png','scutum.png','serpens.png','sextans.png','taurus.png',
+    'telescopium.png','triangulum.png','triangulum australe.png','tucana.png','ursa major.png','ursa minor.png','vela.png','virgo.png','volans.png','vulpecula.png'];
   const constels = document.querySelector('.constels');
 
   fetch('./constellations.json')
     .then(res => res.json())
     .then(data => {
-      data.forEach((c,index) => {
-        const div = document.createElement('div');
-        div.classList.add('row');
-        div.textContent = `${index+1} ) ${represesntations[data.indexOf(c)]} ${c.name}`;
-        constels.appendChild(div);
-        div.addEventListener('click', () => {
-          const details = document.querySelector('.popup-of-constellations');
-          details.style.display = 'flex';
-          const texts=document.querySelector('.popup-of-constellations .parag');
-          const image=document.querySelector('.popup-of-constellations .image');
-          texts.innerHTML = `
-             <h2>${c.name}</h2> 
-            <p><strong>Meaning & Origin:</strong> ${c.meaning_origin}</p>
-            <p><strong>Mythological Story:</strong> ${c.mythological_story}</p>
-            <p><strong>Hemisphere Visibility:</strong> ${c.hemisphere_visibility}</p>
-            <p><strong>Cultural Significance:</strong> ${c.cultural_significance}</p>
-            <p><strong>Modern IAU Boundaries:</strong> ${c.modern_iau_boundaries}</p>
-          `;
-        });
-         
-      });
+      data.forEach((c, index) => {
+  const div = document.createElement('div');
+  div.classList.add('row');
+
+  div.textContent = `${index+1} ) ${represesntations[index]} ${c.name}`;
+  constels.appendChild(div);
+
+  div.addEventListener('click', () => {
+    const details = document.querySelector('.popup-of-constellations');
+    details.style.display = 'flex';
+
+    const texts = document.querySelector('.popup-of-constellations .parag');
+    const image = document.querySelector('.popup-of-constellations .image');
+
+    texts.innerHTML = `
+      <h2>${c.name}</h2> 
+      <p><strong>Meaning & Origin:</strong> ${c.meaning_origin}</p>
+      <p><strong>Mythological Story:</strong> ${c.mythological_story}</p>
+      <p><strong>Hemisphere Visibility:</strong> ${c.hemisphere_visibility}</p>
+      <p><strong>Cultural Significance:</strong> ${c.cultural_significance}</p>
+      <p><strong>Modern IAU Boundaries:</strong> ${c.modern_iau_boundaries}</p>
+    `;
+
+    image.src = `./constellations/${images_hh[index]}`;
+  });
+});
     })
     .catch(err => console.log(err));
 
